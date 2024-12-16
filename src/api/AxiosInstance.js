9ded96e80f2b6df62ea377819cd9e2532236541f
Jithin <jithinpganesh@outlook.com>
2024-12-16 18:49:41 +0530

    axios interceptor done

--- src/api/AxiosInstance.js
+++ src/api/AxiosInstance.js
@@ -5,4 +5,26 @@ const axiosInstance = axios.create({
     withCredentials: true
 });
 
+axiosInstance.interceptors.request.use(
+    config => {
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
+
+axiosInstance.interceptors.response.use(
+    response => response,
+    error => {
+        const { status } = error.response || {};
+        
+        if (status === 401) {
+            window.location.href = '/login';
+        }
+
+        return Promise.reject(error);
+    }
+);
+
 export default axiosInstance;
